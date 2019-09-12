fc0b0a01dce666db2764f37af0194f0a0b20acbc
sandipmohapatra <sandipmohapatra@users.noreply.github.com>
2019-09-12 15:26:28 +0530

    Add files via upload

---
+++ JdbcApp.java
@@ -0,0 +1,95 @@
+import java.sql.*;
+import java.awt.*;
+import java.awt.event.*;
+public class JdbcApp extends Frame implements ActionListener 
+{
+	Label l1,l2,l3;
+	TextField t1,t2,t3;
+Button b1,b2,b3,b4;
+Connection con=null;
+JdbcApp()
+{
+	l1=new Label("empno");
+	l2=new Label("name");
+	l3=new Label("address");
+	t1=new TextField(20);
+	t2=new TextField(20);
+	t3=new TextField(20);
+	b1=new Button("INSERT");
+	b2=new Button("UPDATE");
+	b3=new Button("DELETE");
+	b4=new Button("SELECT");
+	setLayout(new FlowLayout());
+	add(l1);add(t1);
+	add(l2);add(t2);
+	add(l3);add(t3);
+	add(b1);add(b2);add(b3);add(b4);
+}
+public static void main(String[] args)
+{
+	JdbcApp ob=new JdbcApp();
+	ob.setVisible(true);
+	ob.setSize(400,400);
+	
+}
+public void actionPerformed(ActionEvent ae) 
+{
+	   try
+	   {
+	     Class.forName("oracle.jdbc.driver.OracleDriver");
+	      con=DriverManager.getConnection
+	    		  ("jdbc:oracle:thin:@localhost:1521:xe", "system", "1234");
+	   }
+	   catch(Exception ab)
+	   {
+	        ab.printStackTrace();
+	   }
+	        
+	        if(ae.getSource()==(b1))
+	        {
+	        try
+	        {
+	    
+	     PreparedStatement st=con.prepareStatement("insert into mphasisstudent values(?,?,?)");
+	     String x=t1.getText();
+	     String y=t2.getText();
+	     String z=t3.getText();
+	     int p=Integer.parseInt(z);
+	     st.setString(1,x);
+	     st.setString(2,y);
+	     st.setInt(3,p);
+	     st.execute();
+	     System.out.println("one row inserted");
+	   }
+
+	        catch(Exception ab)
+	        {
+	             System.out.println(ab);
+	        }
+	        }
+	        if(ae.getSource()==(b2))
+	        {
+	             try
+	             {
+	   PreparedStatement st=con.prepareStatement("update mphasisstudent set name=?,address=? where empno=?");
+	     String x=t1.getText();
+	     String y=t2.getText();
+	     String z=t3.getText();
+	     int p=Integer.parseInt(x);
+	    st.setString(1,y);
+	     st.setString(2,z);
+	     st.setInt(3,p);
+	     st.execute();
+	     System.out.println("one row updated");
+	             }
+	             catch(Exception rt)
+	             {
+	                  rt.printStackTrace();
+	             }          
+	             
+	        }
+
+	
+}
+
+}
